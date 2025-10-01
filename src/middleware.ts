3c0a809319374f1cae0192aabd22b2cec72f02e3
JuliaOse <josei051@st.ug.edu.gh>
2025-10-01 17:12:27 +0000

    adding protected routes-fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -35,5 +35,11 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    "/dashboard/:path*",
+    "/projects/:path*",
+    "/inbox/:path*",
+    "/profile/:path*",
+  ],
 };
+
