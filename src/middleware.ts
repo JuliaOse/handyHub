de09154de7726c944034a2a6cf3731d51b739802
JuliaOse <josei051@st.ug.edu.gh>
2025-10-01 17:01:30 +0000

    adding protected routes

---
+++ src/middleware.ts
@@ -0,0 +1,39 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+import { createServerClient } from "@supabase/ssr";
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return req.cookies.get(name)?.value;
+        },
+      },
+    }
+  );
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  //too tedious: since all routes are protected ecpet landing page
+
+  //   const protectedRoutes = ["/projects", "/inbox", "/dashboard", "profile", "profile-update"];
+
+  const publicRoutes = ["/", "/landing", "/auth", "/sign-up", "/signup-sp"];
+
+  if (!session && !publicRoutes.includes(req.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL("/auth", req.url));
+  }
+
+  return res;
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
